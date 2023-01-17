043f0648e2d0f30d54796d6076ff30e399f956d0
Darien Perez <dapec_290@hotmail.com>
2023-01-16 22:24:51 -0600

    Cambiar por chamaconas 😍

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world'))
+app.get('/', (req, res) => res.send('Que pasa chamaconas'))
 
 app.listen(8080);
 console.log('Server on port 8080')
